8fee926bc13440463310406738765f702f1405a9
Mila <milamanieva@gmail.com>
2017-09-24 15:03:16 +0200

    1.homework

--- week0/homework/index.js
+++ week0/homework/index.js
@@ -3,15 +3,20 @@ const Person = require("./Person.js");
 // remember to write a constructor function
 let jimmy = new Person("Jimmy", 28);
 
-jimmy.addPhone("55551234");
 
-jimmy.call();
+console.log(jimmy)
+
+console.log(jimmy.addPhone("55551234"));
+
+console.log(jimmy.call());
 // should say "Calling Jimmy at 55551234..."
 
-jimmy.birthday();
+console.log(jimmy.birthday());
 // should say "Wishing Jimmy a happy 29th birthday!"
 
 let jill = new Person("Jill");
 
-jill.call();
+console.log(jill);
+
+console.log(jill.call());
 // should say "Jill has no phone number saved."
