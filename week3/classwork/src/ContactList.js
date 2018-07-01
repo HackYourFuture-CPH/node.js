5add1298ad1b3c51148cdaa483b1fbd6d45ebac7
petcat <catanapetru@gmail.com>
2018-07-01 15:26:56 +0200

    updated ContactList for epxress server

--- week3/classwork/src/ContactList.js
+++ week3/classwork/src/ContactList.js
@@ -1,14 +1,18 @@
-const fs = require("fs");
-const util = require("util");
+// const fs = require("fs");
+import fs from "fs";
+// const util = require("util");
+import util from "util";
+import path from "path";
 
 const writeFile = util.promisify(fs.writeFile);
 const readFile = util.promisify(fs.readFile);
 
-class Contact {
+export class Contact {
     constructor(obj) {
         this.name = obj.name;
         this.age = obj.age;
-    };
+	};
+	
     addPhone(number) {
 
         return this.phoneNumber = number;
@@ -28,24 +32,30 @@ class Contact {
      }
 };
 
-class ContactList {
+export class ContactList {
 	constructor(filename){
 		this.list = [];
 		this.filename = filename;
+		this.load();
 	}
 
 	addContact(contact){
-		if(contact instanceof Contact){
+		if(contact instanceof Contact) {
 			this.list.push(contact);
+			console.log("this.list", this.list);
+			return this.save(this.list);
 		}
 	}
 
-	save(){
-		return writeFile(this.filename, JSON.stringify(this.list), "utf8");
+	save(list){
+		return writeFile(path.resolve(__dirname, this.filename), JSON.stringify(list), "utf8")
+		.then(() => {
+			return list;	
+		});
 	}
 
 	load(){
-		const readFilePromise = readFile(this.filename, "utf8");
+		const readFilePromise = readFile(path.resolve(__dirname, this.filename), "utf8");
 
 		return readFilePromise
 		.then(fileString => {
@@ -65,6 +75,3 @@ class ContactList {
 		// });
 	}
 };
-
-exports.Contact = Contact;
-exports.ContactList = ContactList;
