505c32fb29462ce9efadaa0f3a5d1fdd00057e37
Nathan Kuik <nkuik@toogoodtogo.com>
2020-07-05 09:58:15 +0200

    Ensure baseline file works

--- week4/homework-template/sql_baseline.sql
+++ week4/homework-template/sql_baseline.sql
@@ -1,14 +1,18 @@
-CREATE TABLE `meals` (
+CREATE DATABASE IF NOT EXISTS `mealsharing`;
+
+USE `mealsharing`;
+
+CREATE TABLE `mealsharing`.`meals` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `title` varchar(250) NOT NULL DEFAULT '',
   `maxNumberOfGuests` tinyint(4) NOT NULL DEFAULT '1',
   `description` text,
-  `price` tinyint(5) NOT NULL DEFAULT '0',
+  `price` int(10) NOT NULL DEFAULT '0',
   `createdAt` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (`id`)
 );
 
-CREATE TABLE `reviews` (
+CREATE TABLE `mealsharing`.`reviews` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `mealId` int(11) NULL,
   `content` text,
@@ -18,7 +22,7 @@ CREATE TABLE `reviews` (
   CONSTRAINT `fk_meals_id` FOREIGN KEY (`mealId`) REFERENCES `meals` (`id`) ON DELETE CASCADE
 );
 
-CREATE TABLE `reservations` (
+CREATE TABLE `mealsharing`.`reservations` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `mealId` int(11) NULL,
   `name` varchar(150) NOT NULL DEFAULT '',
@@ -28,14 +32,15 @@ CREATE TABLE `reservations` (
   CONSTRAINT `fk_meals_reservations_id` FOREIGN KEY (`mealId`) REFERENCES `meals` (`id`) ON DELETE CASCADE
 );
 
-INSERT INTO `meals` (`title`, `maxNumberOfGuests`, `description`, `price`)
-  VALUES 
-  ("New Nordic", 30, "Fancy basic food at an extravagant price", 1000);
-
-INSERT INTO `reservations` (`mealId`, `name`, `email`)
+INSERT INTO `mealsharing`.`meals` (`title`, `maxNumberOfGuests`, `description`, `price`)
   VALUES
-  (1, "Franz Ferdinand", "franz@ferdinand.com");
+  ('New Nordic', 30, 'Fancy basic food at an extravagant price', 1000);
 
-INSERT INTO `reviews` (`mealId`, `content`, `numberOfStars`)
+INSERT INTO `mealsharing`.`reservations` (`mealId`, `name`, `email`)
   VALUES
-  (1, "Extremely delicious and simple, but maybe cost a bit too much for some fancy potatoes and dark bread", 4);
+  (1, 'Franz Ferdinand', 'franz@ferdinand.com');
+
+INSERT INTO `mealsharing`.`reviews` (`mealId`, `content`, `numberOfStars`)
+  VALUES
+  (1, 'Extremely delicious and simple, but maybe cost a bit too much for some fancy potatoes and dark bread', 4);
+
