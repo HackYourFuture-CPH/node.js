53e4f976e66fefff9a4e8d1167f02d1426a57d57
Nathan Kuik <nkuik@toogoodtogo.com>
2020-10-25 17:33:59 +0100

    All tests passing

--- week1/exercise-template/src/backend/title_creator.js
+++ week1/exercise-template/src/backend/title_creator.js
@@ -1,1 +1,35 @@
-const createTitle;
+const createTitle = (title) => `
+<head>
+  ${generateCss()}
+  <title>${title}</title>
+</head>
+`;
+
+const generateCss = () => `
+<style type = text/css>
+    .title {
+        background-color: yellow;
+    }
+    .projectUrl {
+        background-color: lightgreen;
+    }
+    .codeUrl {
+        background-color: lightgray;
+    }
+    .previewUrl {
+        background-color: lightpurple;
+    }
+</style>
+`;
+
+// Can also write it like:
+// const createTitle = function(title) {
+//     return `
+// ${generateCss()}
+// <head>
+//   <title>${title}</title>
+// </head>
+// `
+// };
+
+module.exports = createTitle;
