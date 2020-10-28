fa598f05a602e0c623666637f8940269936b3181
behuFTP <benjamin.dals.hughes@gmail.com>
2020-10-28 15:29:46 +0100

    improved exercise

--- week2/exercise-template/src/backend/movies-router.js
+++ week2/exercise-template/src/backend/movies-router.js
@@ -1,7 +1,10 @@
 const express = require("express");
 const router = express.Router();
 
+const movies = require("./movies.json");
+
 router.get("/", async (request, response) => {
+  console.log(movies);
   response.send({ data: [] });
 });
 
