792ff7577d2231dcc542817ead7cb0f2da69c51e
Nathan Kuik <nkuik@toogoodtogo.com>
2020-11-05 16:49:18 +0100

    Week 2

--- week2/exercise-template/src/backend/app.js
+++ week2/exercise-template/src/backend/app.js
@@ -5,6 +5,22 @@ const app = express();
 const moviesRouter = require("./api/movies-router");
 
 // This is where you want to create your is chrome browser middleware (second exercise)
+const middleware = (request, response, next) => {
+    console.log("middlewear root");
+    console.log(request.headers['user-agent']);
+    const isOnChrome = request.headers['user-agent'].includes("Chrome");
+    request.headers.isOnChrome = isOnChrome;
+    console.log(request.headers);
+    next();
+};
+
+app.use('/', middleware);
+app.get('/', (request, response) => {
+    console.log(`is user on chrome?: ${request.headers.isOnChrome}`);
+    response.send("hello world");
+})
+
+app.use('/api/movies', [middleware, moviesRouter]);
 
 // app.use binds middleware to your application. You can give app.use a path and router. The mini router will take care of all requests with the path
 app.use("/api/movies", moviesRouter);
--- week2/teacher-live-coding/src/backend/middleware.js
+++ week2/teacher-live-coding/src/backend/middleware.js
@@ -2,15 +2,18 @@
 // Take a look inside the folder!
 const express = require("express");
 const app = express();
+const miniRouter = require("./mini-router");
 
 app.use((req, res, next) => {
-  console.log(req.headers["accept-language"]);
+  console.log(req.headers);
   const isFromDenmark = req.headers["accept-language"].includes("da");
   console.log(isFromDenmark);
-  req.isFromDenmark = isFromDenmark;
+  request.isFromDenmark = isFromDenmark;
 
   next();
 });
+
+app.use('/router', miniRouter);
 
 app.listen(3000, function () {
   console.log(`> Ready on http://localhost:3000`);
