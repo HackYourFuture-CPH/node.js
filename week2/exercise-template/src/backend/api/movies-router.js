b516dd239e4c2927e021121dc41cb74fd9718750
Nathan Kuik <nkuik@toogoodtogo.com>
2021-02-04 21:16:03 +0100

    All but middleware

--- week2/exercise-template/src/backend/api/movies-router.js
+++ week2/exercise-template/src/backend/api/movies-router.js
@@ -4,8 +4,28 @@ const router = express.Router();
 const movies = require("../data/movies.json");
 
 router.get("/", async (request, response) => {
-  console.log(movies);
-  response.send({ data: [] });
+    let beginYear = parseInt(request.query.beginYear) > 0 ?
+        parseInt(request.query.beginYear) :
+        0;
+    let endYear = parseInt(request.query.endYear) < Number.POSITIVE_INFINITY ?
+        parseInt(request.query.endYear) :
+        Number.POSITIVE_INFINITY;
+    let minRating = parseInt(request.query.minRating) > 0 ?
+        parseInt(request.query.minRating) :
+        0;
+
+    let validMovies = movies.filter(
+        movie => movie.year >= beginYear &&
+            movie.year <= endYear &&
+            movie.rating >= minRating
+    );
+    response.send({ data: validMovies });
+});
+
+router.get("/:year", async (request, response) => {
+    response.send({ data: movies.filter(
+        movie => movie.year === parseInt(request.params.year))
+    });
 });
 
 module.exports = router;
